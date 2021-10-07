84c5b1ef81ed0c69559bee2734f72d9a32911088
Adri <adripalmeromartinez@gmail.com>
2021-10-07 23:40:35 +0200

    _complete_lambdas

--- src/automata/automaton_evaluator.py
+++ src/automata/automaton_evaluator.py
@@ -4,6 +4,7 @@ from typing import Set
 from automata.automaton import FiniteAutomaton, State
 from automata.interfaces import AbstractFiniteAutomatonEvaluator
 
+import queue
 
 class FiniteAutomatonEvaluator(
     AbstractFiniteAutomatonEvaluator[FiniteAutomaton, State],
@@ -15,8 +16,22 @@ class FiniteAutomatonEvaluator(
         raise NotImplementedError("This method must be implemented.")
 
     def _complete_lambdas(self, set_to_complete: Set[State]) -> None:
+        # Breadth-First Search
+        queue = Queue()
+        for item in set_to_complete:
+            queue.put(item)
 
-        raise NotImplementedError("This method must be implemented.")
+        while not queue.empty():
+            state = queue.get()
+            # For each state, search for lambda transitions and add them to the queue
+            for transition in self.transitions:
+                if transition.initial_state == state and transition.symbol == None and transition.final_state not in set_to_complete:
+                    # Add to set completed with lambdas and push to queue
+                    set_to_complete.add(transition.final_state)
+                    queue.put(transition.final_state)
+                else:
+                    continue
+        return
 
     def is_accepting(self) -> bool:
 
