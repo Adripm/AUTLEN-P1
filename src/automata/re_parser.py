427da4a8aa6d3d52bfb95eca59ab8c3cc005b6a4
Adri <adripalmeromartinez@gmail.com>
2021-10-14 22:13:50 +0200

    create automaton star

--- src/automata/re_parser.py
+++ src/automata/re_parser.py
@@ -1,16 +1,22 @@
 """Conversion from regex to automata."""
-from automata.automaton import FiniteAutomaton
+from automata.automaton import FiniteAutomaton, State, Transition
 from automata.re_parser_interfaces import AbstractREParser
 
 
 class REParser(AbstractREParser):
     """Class for processing regular expressions in Kleene's syntax."""
 
+    # Utility
+    # Returns the current state count as a string and plus ones
+    def _add_state(self) -> str:
+        self.state_counter+=1
+        return 'state'+str(self.state_counter-1)
+
     def _create_automaton_empty(
         self,
     ) -> FiniteAutomaton:
 
-        initial_state = State(name='final', is_final=True)
+        initial_state = State(name=self._add_state(), is_final=True)
         states: Collection[State] = {initial_state} # Contains only one state
         symbols: Collection[str] = {} # is empty
         transitions: Collection[Transition] = {} # is empty
@@ -26,8 +32,8 @@ class REParser(AbstractREParser):
         self,
     ) -> FiniteAutomaton:
 
-        state1 = State(name='initial', is_final=False)
-        state2 = State(name='final', is_final=True)
+        state1 = State(name=self._add_state(), is_final=False)
+        state2 = State(name=self._add_state(), is_final=True)
         transition1 = Transition(initial_state=state1, symbol=None, final_state=state2)
 
         initial_state = state1
@@ -47,8 +53,8 @@ class REParser(AbstractREParser):
         symbol: str,
     ) -> FiniteAutomaton:
 
-        state1 = State(name='initial', is_final=False)
-        state2 = State(name='final', is_final=True)
+        state1 = State(name=self._add_state(), is_final=False)
+        state2 = State(name=self._add_state(), is_final=True)
         transition1 = Transition(initial_state=state1, symbol=symbol, final_state=state2)
 
         initial_state = state1
@@ -67,8 +73,39 @@ class REParser(AbstractREParser):
         self,
         automaton: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        # raise NotImplementedError("This method must be implemented.")
-        return None
+
+        initial_state = State(name=self._add_state(), is_final=False)
+        final_state = State(name=self._add_state(), is_final=True)
+
+        states: Collection[State] = automaton.states
+        symbols: Collection[str] = automaton.symbols
+        transitions: Collection[Transition] = automaton.transitions
+
+        # For each final state in inner automaton, connect to new final state with lambda transitions
+        # Add transition to inner automaton initial state
+        # Final states in inner automaton are no longer final
+        for state in automaton.states:
+            if state.is_final:
+                state.is_final = False
+                transitions.add(Transition(initial_state=state, symbol=None, final_state=final_state))
+                transitions.add(Transition(initial_state=state, symbol=None, final_state=automaton.initial_state))
+
+        # Add new states to automaton
+        states.add(initial_state)
+        states.add(final_state)
+
+        # Add transition from new initial state to inner automaton initial state
+        transitions.add(Transition(initial_state=initial_state, symbol=None, final_state=automaton.initial_state))
+
+        # Add transition from new initial state to final state
+        transitions.add(Transition(initial_state=initial_state, symbol=None, final_state=final_state))
+
+        return FiniteAutomaton(
+            initial_state = initial_state,
+            states = states,
+            symbols = symbols,
+            transitions = transitions
+        )
 
     def _create_automaton_union(
         self,
